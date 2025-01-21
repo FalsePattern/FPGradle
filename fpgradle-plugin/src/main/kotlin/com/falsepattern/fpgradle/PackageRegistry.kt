fd26b05f64df554d01c499885abed7c8f3ce56ec
FalsePattern <me@falsepattern.com>
2025-01-21 17:27:54 +0100

    update jetbrains annotations and lwjgl3ify

--- fpgradle-plugin/src/main/kotlin/com/falsepattern/fpgradle/PackageRegistry.kt
+++ fpgradle-plugin/src/main/kotlin/com/falsepattern/fpgradle/PackageRegistry.kt
@@ -24,7 +24,7 @@
 package com.falsepattern.fpgradle
 
 internal object PackageRegistry {
-    const val JETBRAINS_ANNOTATIONS = "org.jetbrains:annotations:24.1.0"
+    const val JETBRAINS_ANNOTATIONS = "org.jetbrains:annotations:26.0.1"
     const val MIXINS_GROUP = "com.github.LegacyModdingMC.UniMixins"
     const val MIXINS_MODULE = "unimixins-all-1.7.10"
     const val MIXINS_VERSION = "0.1.19"
@@ -34,7 +34,7 @@ internal object PackageRegistry {
     const val MIXINS_AP_GUAVA = "com.google.guava:guava:24.1.1-jre"
     const val MIXINS_AP_GSON = "com.google.code.gson:gson:2.8.6"
     const val RFB_VERSION = "1.0.7"
-    const val LWJGL3IFY_VERSION = "2.1.5"
+    const val LWJGL3IFY_VERSION = "2.1.6"
     const val MODERN_JAVA_ASM_VERSION = "9.7.1"
     const val MODERN_JAVA_COMMONS_LANG = "org.apache.commons:commons-lang3:3.14.0"
     const val MODERN_JAVA_COMMONS_COMPRESS = "org.apache.commons:commons-compress:1.26.2"
