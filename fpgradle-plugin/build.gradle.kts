598241a61b311fd2573fb7e58dff9dbfe2dc7fab
FalsePattern <me@falsepattern.com>
2025-05-26 12:55:30 +0200

    0.16.1

--- fpgradle-plugin/build.gradle.kts
+++ fpgradle-plugin/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("com.gradleup.shadow") version "9.0.0-beta13"
 }
 
-val buildscriptVersion = "0.16.0"
+val buildscriptVersion = "0.16.1"
 
 group = "com.falsepattern"
 version = buildscriptVersion
