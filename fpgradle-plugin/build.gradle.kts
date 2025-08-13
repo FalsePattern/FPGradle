2c47a37068ea67d092193d08f43c397773eb8b91
FalsePattern <me@falsepattern.com>
2025-08-13 21:01:42 +0200

    ci: 1.1.3

--- fpgradle-plugin/build.gradle.kts
+++ fpgradle-plugin/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("com.gradleup.shadow") version "9.0.1"
 }
 
-val buildscriptVersion = "1.1.2"
+val buildscriptVersion = "1.1.3"
 
 group = "com.falsepattern"
 version = buildscriptVersion
